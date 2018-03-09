028e62756b342619c0a5bbd22f62074e95750a05
blozach <bastien.lozach@gmail.com>
2018-03-09 12:30:46 +0100

    regex

--- grammar/Token.js
+++ grammar/Token.js
@@ -1,3 +1,19 @@
+const token = {
+	"newline" : "\n",
+	"tab" : "(\t)|( {4})",
+	"var" : "$.*",
+	"comment" : "(//.*)|(/\*.*\*/)",
+	"string" : "\".*\"",
+	"open_accol" : "{",
+	"close_accol" : "}",
+	"open_parent" : "(",
+	"close_parent" : ")",
+	"identifier" : ".+",
+	"operator" : "[(\+)(\-)(\*)(\.)(\/)(\=)(\<)(\>)]+",
+	"number" : "[0-9\.]+"	
+};
+
+
 class Token {
 	constructor(type, value = null, pos = null) {
 		if (!type) throw new Error("a token requires a type")
