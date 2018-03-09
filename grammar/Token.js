a37ac36cdf5bcf7bc8bc58af9c3cd08add0eb60c
blozach <bastien.lozach@gmail.com>
2018-03-09 13:00:49 +0100

    Ajout d'un constructeur token(word, pos)

--- grammar/Token.js
+++ grammar/Token.js
@@ -1,16 +1,34 @@
 const token = {
-	"newline" : "\n",
-	"tab" : "(\t)|( {4})",
-	"var" : "$.*",
-	"comment" : "(//.*)|(/\*.*\*/)",
-	"string" : "\".*\"",
-	"open_accol" : "{",
-	"close_accol" : "}",
-	"open_parent" : "(",
-	"close_parent" : ")",
-	"identifier" : ".+",
-	"operator" : "[(\+)(\-)(\*)(\.)(\/)(\=)(\<)(\>)]+",
-	"number" : "[0-9\.]+"	
+	//commentaire et chaines
+	"comment" : /^(\/\/.*)|(\/\*.*\*\/)/,
+	"string" : /^(\".*\")|(\'.*\')/,
+	
+	//whitespace
+	"newline" : /^\n/,
+	"tab" : /^(\t)|( {4})/,
+
+	//semi-colon
+	"semi-colon" : /^;/,
+
+	//parentheses et accolades
+	"open_accol" : /^{/,
+	"close_accol" : /^}/,
+	"open_parent" : /^(/,
+	"close_parent" : /^)/,
+	
+	//operateurs
+	"binair_operator" : /^[(\+)(\-)(\*)(\.)(\/)(\=)(\<)(\>)(\!)]+/,
+	"unair_operator" : /^[\!]+/,
+	
+	//variables et valeurs
+	"var" : /^\$.+/,
+	"number" : /^[0-9\.]+/,
+
+	//identifier (fonctions, noms de classes...)
+	"identifier" : /^[a-zA-Z]+/,
+
+	//tout le reste
+	"error" : /./
 };
 
 
@@ -21,6 +39,26 @@ class Token {
 		this.type = type
 		this.value = value
 		this.pos = pos
+	}
+
+	constructor(word, pos = null){
+		this.pos = pos,
+		this.value = word
+		this.type = "";
+		keys = Object.keys(token);
+		size = keys.length
+		i = 0 ;
+		while (i < size && this.type == "" ){
+			if( word.match(token[keys[i]]) != null ){
+				this.type = keys[i]
+			}
+			i++
+		}
+		
+	}
+
+	static get token(){
+		return token;
 	}
 }
 
